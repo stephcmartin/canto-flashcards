bf607782504b8123098583c28cb372b7f4739e31
Stephanie Martin Connelly <martin.stephaniechow@gmail.com>
2018-12-07 15:31:56 -0500

    Adding build files.

---
+++ build/precache-manifest.18e660ea7cc8e864527001522c47d13b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a487813fac0e90d3e0c",
+    "url": "/static/js/main.1a487813.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "1a487813fac0e90d3e0c",
+    "url": "/static/css/main.2fe81749.chunk.css"
+  },
+  {
+    "revision": "8ef71a0dfd269c63b64ecb55f512a663",
+    "url": "/index.html"
+  }
+];
